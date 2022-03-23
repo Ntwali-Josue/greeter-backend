3654fde6324fa8e52f52eefd6a7f6cac4fc039db
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-03-23 13:24:22 +0200

    Add and fix linters

--- test/controllers/api/v1/greeting_controller_test.rb
+++ test/controllers/api/v1/greeting_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class Api::V1::GreetingControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get api_v1_greeting_index_url
     assert_response :success
   end
